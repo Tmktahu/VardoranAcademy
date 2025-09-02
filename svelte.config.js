a7b15a83a96d0d62840b34d489d8190a0ed9ffb0
Travis Kehrli <tmktahu@gmail.com>
2025-09-02 03:40:24 -0700

    tweaking svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,24 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import preprocess from 'svelte-preprocess';
+import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: preprocess({ postcss: true, typescript: true }),
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : ''
+		},
+		alias: {
+			'@/*': './src/lib/*',
+			$lib: resolve('./src/lib')
+		}
 	}
 };
 
